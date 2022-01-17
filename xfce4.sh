e3496a330b769dd3d5badd73813a61ed2a333c14
GitHub059 <97761167+GitHub059@users.noreply.github.com>
2022-01-17 15:39:28 -0300

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -1,3 +1,13 @@
-apt install xfce4 tigervnc-standalone-server xfce4-terminal firefox
-vncserver :1
-./ngrok tcp 5901 --region sa
+echo instalando o ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+./ngrok authtoken 23o7Blb79YJhsxhQM0s3JvPcCbb_4npfySab6kus3MpsjhYmE
+echo instalando o xfce4 e outros pacotes
+apt install xfce4 xfce4-taskmanager firefox xfce4-terminal tightvnc-standalone-server
+echo configurando a senha
+wget https://github.com/GitHub059/VPS-googlecolab/blob/main/vnc.zip?raw=true
+apt install zip; unzip vnc.zip
+vncserver :1 -geometry 1280x720
+./ngrok tcp 5901 
+echo seu ip:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
