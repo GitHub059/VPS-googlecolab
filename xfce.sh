1362905131d5fecb98302bcd99002bebc78b53d1
GitHub059 <97761167+GitHub059@users.noreply.github.com>
2022-01-18 02:16:38 +0000

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -5,6 +5,7 @@ unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1;
 ./ngrok authtoken 23o7Blb79YJhsxhQM0s3JvPcCbb_4npfySab6kus3MpsjhYmE;
 echo **instalando o xfce4 e outros pacotes**
 apt install xfce4 xfce4-taskmanager firefox xfce4-terminal tightvncserver
+export USER=root
 vncserver :1 -geometry 1280x720
 ./ngrok tcp 5901
 echo seu ip:
